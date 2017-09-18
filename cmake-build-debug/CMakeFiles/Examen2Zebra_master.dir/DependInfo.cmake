dc30b4d7e46ea01869bf5179bd9143e75b03db67
Oscarluiis <tinocoscar11@unitec.edu>
2017-09-18 08:59:06 -0600

    ...

---
+++ cmake-build-debug/CMakeFiles/Examen2Zebra_master.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Nerea/Downloads/Examen2Zeb/Evaluador.cpp" "/Users/Nerea/Downloads/Examen2Zeb/cmake-build-debug/CMakeFiles/Examen2Zebra_master.dir/Evaluador.cpp.o"
+  "/Users/Nerea/Downloads/Examen2Zeb/Libro.cpp" "/Users/Nerea/Downloads/Examen2Zeb/cmake-build-debug/CMakeFiles/Examen2Zebra_master.dir/Libro.cpp.o"
+  "/Users/Nerea/Downloads/Examen2Zeb/NodoBinario.cpp" "/Users/Nerea/Downloads/Examen2Zeb/cmake-build-debug/CMakeFiles/Examen2Zebra_master.dir/NodoBinario.cpp.o"
+  "/Users/Nerea/Downloads/Examen2Zeb/main.cpp" "/Users/Nerea/Downloads/Examen2Zeb/cmake-build-debug/CMakeFiles/Examen2Zebra_master.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Examen2Zebra_master.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Examen2Zebra_master.dir/Evaluador.cpp.o"
+  "CMakeFiles/Examen2Zebra_master.dir/Libro.cpp.o"
+  "CMakeFiles/Examen2Zebra_master.dir/main.cpp.o"
+  "CMakeFiles/Examen2Zebra_master.dir/NodoBinario.cpp.o"
+  "Examen2Zebra_master.pdb"
+  "Examen2Zebra_master"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Examen2Zebra_master.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
